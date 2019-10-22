c109d54a42c1e65064518bd73064b42bd7801c5f
Michael Washington <Washington_Michael@lacoe.edu>
2019-10-22 12:27:36 -0700

    Fixes #12

---
+++ GTAVehicles/Models/IdentityUserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTAVehicles.Models
+{
+    public class IdentityUserDTO
+    {
+        public virtual string Id { get; set; }
+        public virtual string IdentityUserUserName { get; set; }
+        public virtual string SecurityStamp { get; set; }
+        public virtual bool PhoneNumberConfirmed { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string NormalizedUserName { get; set; }
+        public virtual string NormalizedEmail { get; set; }
+        public virtual DateTimeOffset? LockoutEnd { get; set; }
+        public virtual bool LockoutEnabled { get; set; }
+        public virtual bool EmailConfirmed { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string ConcurrencyStamp { get; set; }
+        public virtual int AccessFailedCount { get; set; }
+        public virtual bool TwoFactorEnabled { get; set; }
+    }
+}
