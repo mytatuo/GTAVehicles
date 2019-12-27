4afd74965190a8a01fc5aab79ae5498edb929a67
Michael Washington <Washington_Michael@lacoe.edu>
2019-12-27 13:01:55 -0800

    Allows Characters to be selected

---
+++ GTAVehicles/Models/GtacharactersDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTAVehicles.Models
+{
+    public class GtacharactersDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
