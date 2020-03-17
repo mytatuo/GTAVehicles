8a297ccaf2419099e0687c57365363ef11e06a9a
Michael Washington <Washington_Michael@lacoe.edu>
2020-03-16 17:17:07 -0700

    [sp_GTAVehiclesRankedByUser] stored procedure returns the first character that owns a vehicle

--- GTAVehicles/!SQL/01.20.00.sql
+++ GTAVehicles/!SQL/01.20.00.sql
@@ -1,11 +1,9 @@
 ﻿SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-CREATE PROCEDURE [dbo].[sp_GTAVehiclesRankedByUser]
+ALTER PROCEDURE [dbo].[sp_GTAVehiclesRankedByUser]
 @UserName nvarchar(500)
 AS
 BEGIN
@@ -22,7 +20,16 @@ dbo.GTAVehicles.DragSpeed,
 CAST(RANK() OVER (ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRank,
 CAST(RANK() OVER (PARTITION BY dbo.GTAVehicles.ClassID ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRankInClass,
 GTAPlayers.UserName,
-'' AS CharactersOwningVehicle
+(
+SELECT       top 1 GTAPlayerCharacters.CharacterName
+FROM            GTAVehicleClass INNER JOIN
+                         GTAVehicles GTAVehiclesSubQuery ON GTAVehicleClass.ID = GTAVehiclesSubQuery.ClassID LEFT OUTER JOIN
+                         GTAPlayerVehicles ON GTAVehiclesSubQuery.ID = GTAPlayerVehicles.VehicleID LEFT OUTER JOIN
+                         GTAPlayerGarages LEFT OUTER JOIN
+                         GTAPlayerCharacters ON GTAPlayerGarages.CharacterID = GTAPlayerCharacters.ID LEFT OUTER JOIN
+                         GTAPlayers GTAPlayersSubQuery ON GTAPlayerCharacters.PlayerID = GTAPlayersSubQuery.ID ON GTAPlayerVehicles.PlayerGarageID = GTAPlayerGarages.ID
+WHERE        (GTAPlayersSubQuery.UserName = GTAPlayers.UserName) AND (GTAVehiclesSubQuery.ID = dbo.GTAVehicles.ID)
+) AS CharactersOwningVehicle
 FROM            GTAVehicleClass INNER JOIN
                          GTAVehicles ON GTAVehicleClass.ID = GTAVehicles.ClassID LEFT OUTER JOIN
                          GTAPlayerVehicles ON GTAVehicles.ID = GTAPlayerVehicles.VehicleID LEFT OUTER JOIN
@@ -31,4 +38,3 @@ FROM            GTAVehicleClass INNER JOIN
                          GTAPlayers ON GTAPlayerCharacters.PlayerID = GTAPlayers.ID ON GTAPlayerVehicles.PlayerGarageID = GTAPlayerGarages.ID
 WHERE        (GTAPlayers.UserName = @UserName) or (GTAPlayers.UserName is null) 
 END
-GO
