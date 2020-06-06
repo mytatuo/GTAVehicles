e4ac3b6d3c23ee38efdac78541be309de3db2651
Michael Washington <webmaster@adefwebserver.com>
2020-06-06 10:56:26 -0700

    Solves #21

--- GTAVehicles/!SQL/01.20.00.sql
+++ GTAVehicles/!SQL/01.20.00.sql
@@ -4,23 +4,62 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-ALTER PROCEDURE [dbo].[sp_GTAVehiclesRankedByUser]
+CREATE PROCEDURE [dbo].[sp_GTAVehiclesRankedByUser]
 @UserName nvarchar(500)
 AS
 BEGIN
 	SET NOCOUNT ON;
+
+    DECLARE @VehiclesRanked table
+	(
+	ID int NOT NULL, 
+	VehicleModel nvarchar(500) NOT NULL, 
+	ClassID int NOT NULL, 
+	ClassName nvarchar(500) NOT NULL,
+	TrackSpeed [nvarchar](255) NULL, 
+	TrackRank int NOT NULL, 
+    TrackRankInClass int NOT NULL,
+    DragSpeed [money] NULL, 
+	DragRank int NOT NULL, 
+    DragRankInClass int NOT NULL
+	PRIMARY KEY (ID) -- This key helps performance or UNIQUE CLUSTERED as used below
+	--UNIQUE CLUSTERED (TransactionID, ProductID, TransactionDate)
+	)
+
+	Insert Into @VehiclesRanked
+	(
+	ID,
+	VehicleModel, 
+	ClassID, 
+	ClassName,
+	TrackSpeed, 
+	TrackRank, 
+    TrackRankInClass,
+    DragSpeed, 
+	DragRank, 
+    DragRankInClass
+	)
+    SELECT        
+	dbo.GTAVehicles.ID, dbo.GTAVehicles.VehicleModel, dbo.GTAVehicles.ClassID, dbo.GTAVehicleClass.ClassName, dbo.GTAVehicles.TrackSpeed, 
+	CAST(RANK() OVER (ORDER BY dbo.GTAVehicles.TrackSpeed ASC) AS INT) AS TrackRank, 
+	CAST(RANK() OVER (PARTITION BY dbo.GTAVehicles.ClassID ORDER BY dbo.GTAVehicles.TrackSpeed ASC) AS INT) AS TrackRankInClass, 
+	dbo.GTAVehicles.DragSpeed, 
+	CAST(RANK() OVER (ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRank, 
+	CAST(RANK() OVER (PARTITION BY dbo.GTAVehicles.ClassID ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRankInClass
+	FROM            
+	dbo.GTAVehicleClass INNER JOIN dbo.GTAVehicles ON dbo.GTAVehicleClass.ID = dbo.GTAVehicles.ClassID
+
 SELECT DISTINCT        
 dbo.GTAVehicles.ID,
 dbo.GTAVehicles.VehicleModel,
 dbo.GTAVehicles.ClassID,
 dbo.GTAVehicleClass.ClassName, 
 dbo.GTAVehicles.TrackSpeed,
-CAST(RANK() OVER (ORDER BY dbo.GTAVehicles.TrackSpeed ASC) AS INT) AS TrackRank,
-CAST(RANK() OVER (PARTITION BY dbo.GTAVehicles.ClassID ORDER BY dbo.GTAVehicles.TrackSpeed ASC) AS INT) AS TrackRankInClass,
+(select TrackRank from @VehiclesRanked where ID = dbo.GTAVehicles.ID) AS TrackRank,
+(select TrackRankInClass from @VehiclesRanked where ID = dbo.GTAVehicles.ID) AS TrackRankInClass,
 dbo.GTAVehicles.DragSpeed,
-CAST(RANK() OVER (ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRank,
-CAST(RANK() OVER (PARTITION BY dbo.GTAVehicles.ClassID ORDER BY dbo.GTAVehicles.DragSpeed DESC) AS INT) AS DragRankInClass,
+(select DragRank from @VehiclesRanked where ID = dbo.GTAVehicles.ID) AS DragRank,
+(select DragRankInClass from @VehiclesRanked where ID = dbo.GTAVehicles.ID) AS DragRankInClass,
 GTAPlayers.UserName,
 
 STUFF((SELECT ';' + 
